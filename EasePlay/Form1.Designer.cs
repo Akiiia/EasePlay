1384e0beff28b239d350d9ccbf3fae3907bec573
a <3811118@163.com>
2024-08-26 16:29:56 +0800

    添加项目文件。

---
+++ EasePlay/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace EasePlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.trackBarProgress = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarProgress)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.buttonOpen);
+            this.flowLayoutPanel1.Controls.Add(this.buttonPlay);
+            this.flowLayoutPanel1.Controls.Add(this.buttonPause);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 38);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(3, 3);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "打开";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(84, 3);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "播放";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(165, 3);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 23);
+            this.buttonPause.TabIndex = 2;
+            this.buttonPause.Text = "暂停";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 340);
+            this.panel1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // trackBarProgress
+            // 
+            this.trackBarProgress.LargeChange = 1;
+            this.trackBarProgress.Location = new System.Drawing.Point(12, 402);
+            this.trackBarProgress.Maximum = 100;
+            this.trackBarProgress.Name = "trackBarProgress";
+            this.trackBarProgress.Size = new System.Drawing.Size(776, 45);
+            this.trackBarProgress.TabIndex = 2;
+            this.trackBarProgress.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarProgress.Scroll += new System.EventHandler(this.trackBarProgress_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBarProgress);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarProgress)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TrackBar trackBarProgress;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ EasePlay/Form1.cs
@@ -0,0 +1,99 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasePlay
+{
+    public partial class Form1 : Form
+    {
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Core.Initialize();
+            // 初始化 LibVLC 核心
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            _mediaPlayer.Hwnd = panel1.Handle;
+            // 设置视频输出到 panel
+
+            timer1.Interval = 1000;
+            // 每秒更新一次进度
+            timer1.Tick += Timer1_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "视频文件|*.mp4;*.avi;*.mkv|所有文件|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var media = new Media(_libVLC, new Uri(openFileDialog.FileName));
+                    _mediaPlayer.Media = media;
+
+                    if (_mediaPlayer.Media != null)
+                    {
+                        _mediaPlayer.Play();
+                        timer1.Start();
+                    }
+                }
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            if (_mediaPlayer.Media != null)
+            {
+                _mediaPlayer.Play();
+                timer1.Start();
+            }
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if (_mediaPlayer.IsPlaying)
+            {
+                _mediaPlayer.Pause();
+                timer1.Stop();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (_mediaPlayer.Length > 0)
+            {
+                trackBarProgress.Value = (int)(_mediaPlayer.Time * 100 / _mediaPlayer.Length);
+                // 更新进度条
+            }
+        }
+
+        private void trackBarProgress_Scroll(object sender, EventArgs e)
+        {
+            if (_mediaPlayer.Length > 0)
+            {
+                _mediaPlayer.Time = (long)(_mediaPlayer.Length * trackBarProgress.Value / 100);
+                // 当 _mediaPlayer.Time 被赋值为某个时间点时，视频将跳转到该时间点开始播放
+                // 我第一次看到这段代码的时候还怀疑是写反了，为什么是给 _mediaPlayer 赋值？进一步了解之后就理解了
+            }
+        }
+
+    }
+}
